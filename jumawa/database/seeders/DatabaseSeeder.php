9e5eb1f2639d676d41aba3dca8e3c908a2a5cd85
Darren <derren.ariyo.jt@gmail.com>
2026-05-27 18:06:48 +0700

    Update DatabaseSeeder.php

--- jumawa/database/seeders/DatabaseSeeder.php
+++ jumawa/database/seeders/DatabaseSeeder.php
@@ -19,7 +19,8 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'email' => 'derren@gmail.com',
+            'password' => bcrypt('derren'),
         ]);
     }
 }
